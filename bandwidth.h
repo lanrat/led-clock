13414ae138a92d1151c722d640b500375d8ced68
Ian Foster <mrlanrat@gmail.com>
2016-05-25 10:01:20 -0700

    switched to pfSense SNMP

--- bandwidth.h
+++ bandwidth.h
@@ -1,10 +1,13 @@
 #ifndef BANDWIDTH_H
 #define BANDWIDTH_H
 
-// DD-WRT
 #define SNMP_HOST "192.168.1.1"
-#define SNMP_WAN_UP_OID "iso.3.6.1.2.1.2.2.1.10.3"
-#define SNMP_WAN_DN_OID "iso.3.6.1.2.1.2.2.1.16.3"
+// DD-WRT
+//#define SNMP_WAN_UP_OID "iso.3.6.1.2.1.2.2.1.10.3"
+//#define SNMP_WAN_DN_OID "iso.3.6.1.2.1.2.2.1.16.3"
+// pfSense
+#define SNMP_WAN_UP_OID "iso.3.6.1.2.1.2.2.1.10.1"
+#define SNMP_WAN_DN_OID "iso.3.6.1.2.1.2.2.1.16.1"
 #define OID_LEN 11
 #define COMMUNITY "public"
 
