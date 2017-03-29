aa19112259d91fee091b6b79f27ba8266444ae42
Ian Foster <mrlanrat@gmail.com>
2017-03-28 19:50:57 -0700

    updates and better weather

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./clock >> clock.log
+./clock >> clock.log 2>> clock.err
--- weather.cc
+++ weather.cc
@@ -6,9 +6,11 @@
 #include "http.h"
 #include "weather.h"
 
-using namespace std;
+// "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="sanfrancisco, ca")&format=xml"
+static auto URL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%2294121%22)&format=xml";
 
-static auto URL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22sanfrancisco%2C%20ca%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+
+using namespace std;
 
 static CURL *conn;
 
